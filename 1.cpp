1ecbb4fe257884cea600df9ddcbb12ea12ff3972
volopl <besurviving@QQ.COM>
2024-10-31 00:00:18 +0800

    添加项目文件。

---
+++ 1.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+int main()
+{
+	int n, k;
+	scanf("%d%d",&n,&k);
+
+	int* a = (int*)malloc(n * sizeof(int));
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", a+i);
+	}
+
+	for(int i=0;i<n-1;i++)
+		for (int j = i,temp;j < n; j++)
+		{
+			if (a[i] > a[j])
+			{
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+
+		}
+
+	for (int i = 1,min=a[0],count=1; i < n; i++)
+	{
+		if (a[i] > min)
+		{
+			min = a[i];
+			count++;
+
+			if (count == 3)
+				printf("%d",min);
+		}
+
+	}
+
+
+
+	free(a);
+}
