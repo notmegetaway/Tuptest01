6ccda3629df98b83125e11c6dee8e3c386fd77ad
volopl <besurviving@QQ.COM>
2024-10-31 19:55:08 +0800

    1031

--- 2.cpp
+++ 2.cpp
@@ -1,8 +1,21 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
-
-int main()
-{
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//
+//
+//int main()
+//{
+//	int sum;
+//	double fare;
+//
+//	scanf("%d",&sum);
+//
+//	if (sum <= 150)
+//		fare = sum * 0.4463;
+//	else if (sum <= 400)
+//		fare = 150 * 0.4463 + (sum - 150) * 0.4663;
+//	else
+//		fare = 150 * 0.4463 + 250 * 0.4663 + (sum - 400) * 0.5663;
+//
+//	printf("%.11lf",fare);
+//}
